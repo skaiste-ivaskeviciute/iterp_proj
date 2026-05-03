25235440952f2891e9ed65bd15fb66b6116badec
skaiste-ivaskeviciute <151399097+skaiste-ivaskeviciute@users.noreply.github.com>
2026-05-03 19:50:39 +0000

    Add MDK-ARM folder via upload

---
+++ STM32L073/MDK-ARM/STM32L073/statechart.d
@@ -0,0 +1,4 @@
+./stm32l073/statechart.o: ..\..\statechart\src-gen\Statechart.c \
+  ..\..\statechart\src-gen\..\src\sc_types.h \
+  ..\..\statechart\src-gen\Statechart.h \
+  ..\..\statechart\src-gen\Statechart_required.h
